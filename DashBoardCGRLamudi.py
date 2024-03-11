0410bb9cf77fbd5c8276e0c61b9e429847dff1c7
Muhammad Yasin <134952750+jasim76@users.noreply.github.com>
2024-03-11 22:07:34 +0700

    Update DashBoardCGRLamudi.py

--- DashBoardCGRLamudi.py
+++ DashBoardCGRLamudi.py
@@ -3,7 +3,7 @@ import pickle
 import altair as alt
 import numpy as np
 import pandas as pd
-from sklearn.model_selection import train_test_split
+# from sklearn.model_selection import train_test_split
 from sklearn.linear_model import LinearRegression
 
 st.set_page_config(layout='wide')
